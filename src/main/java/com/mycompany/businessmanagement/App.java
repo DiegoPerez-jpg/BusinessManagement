3e757a4ec772988ce5929c6dbcba2b5d84b87d1a
Alejandro <retrogrado112@gmail.com>
2025-11-21 21:24:48 +0100

    Interfaz

--- src/main/java/com/mycompany/businessmanagement/App.java
+++ src/main/java/com/mycompany/businessmanagement/App.java
@@ -18,6 +18,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("primary"), 640, 480);
+        stage.setTitle("Business Management Alejandro del Castillo, Diego Pérez, Alejandro Martín");
         stage.setScene(scene);
         stage.show();
     }
