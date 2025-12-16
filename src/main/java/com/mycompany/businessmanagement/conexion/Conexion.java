d7041e01ca2a6977cdb21187adf1c024417bc166
diego <diegopg2005@gmail.com>
2025-12-16 12:09:31 +0100

    xd

--- src/main/java/com/mycompany/businessmanagement/conexion/Conexion.java
+++ src/main/java/com/mycompany/businessmanagement/conexion/Conexion.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 public class Conexion {
 
     private static final String URL = "jdbc:mysql://localhost:3306/businessmanagement";
-    private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String USER = "usuario";
+    private static final String PASSWORD = "1234";
 
 
     static {
